690a7b53ceeb9153d4cadbf72f84917f94be0500
Jakub Szuppe <jakub@streamcomputing.eu>
2017-07-05 10:36:17 +0200

    Basic Philox4x32_10 implementation
    Performance untested, one state, ordering matches
    the ordering of a serial Philox4x32_10 generator (with
    one exception: each generation starts from a position
    that is a mutiple of 4).

--- library/src/rng/philox4x32_10_state.hpp
+++ library/src/rng/philox4x32_10_state.hpp
@@ -25,6 +25,54 @@ struct rocrand_philox4_32_10_state
 {
     uint4 counter;
     uint2 key;
+
+    __host__ __device__
+    rocrand_philox4_32_10_state()
+        : counter({0, 0, 0, 0}), key({0, 0})
+    {
+
+    }
+
+    __host__ __device__
+    rocrand_philox4_32_10_state(unsigned long long seed)
+        : counter({0, 0, 0, 0})
+    {
+       key.x = static_cast<unsigned int>(seed);
+       key.y = static_cast<unsigned int>(seed >> 32);
+    }
+
+    inline __host__ __device__
+    void discard(unsigned int n)
+    {
+        uint4 temp = counter;
+        counter.x += n;
+        counter.y += (counter.x < temp.x ? 1 : 0);
+        counter.z += (counter.y < temp.y ? 1 : 0);
+        counter.w += (counter.z < temp.z ? 1 : 0);
+    }
+
+    inline __host__ __device__
+    void discard()
+    {
+        if(counter.x++ == 0) return;
+        if(counter.y++ == 0) return;
+        if(counter.z++ == 0) return;
+        counter.w++;
+    }
+
+    inline __host__
+    void reset()
+    {
+        counter = {0, 0, 0, 0};
+    }
+
+    inline __host__
+    void set_seed(unsigned long long seed)
+    {
+        key.x = static_cast<unsigned int>(seed);
+        key.y = static_cast<unsigned int>(seed >> 32);
+        reset();
+    }
 };
 
 #endif // ROCRAND_RNG_PHILOX4X32_10_STATE_H_
