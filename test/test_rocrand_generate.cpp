bf1fc1eb55ed86cabffad99ba68a83460f89a73a
Jakub Szuppe <jakub@streamcomputing.eu>
2017-07-06 10:00:17 +0200

    Implement rocrand_generate()
    [ROCm/rocRAND commit: 99e68a99a87e0378b91ce203c1883386ac8a704e]

---
+++ test/test_rocrand_generate.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+#include <hip/hip_runtime.h>
+#include <rocrand.h>
+
+TEST(rocrand_generate_tests, simple_test)
+{
+    rocrand_generator generator;
+    ASSERT_EQ(
+        rocrand_create_generator(
+            &generator,
+            ROCRAND_RNG_PSEUDO_PHILOX4_32_10
+        ),
+        ROCRAND_STATUS_SUCCESS
+    );
+
+    const size_t size = 256;
+    unsigned int * data;
+    ASSERT_EQ(
+        hipMalloc((void **)&data, size * sizeof(unsigned int)),
+        hipSuccess
+    );
+    ASSERT_EQ(hipDeviceSynchronize(), hipSuccess);
+
+    EXPECT_EQ(
+        rocrand_generate(generator, (unsigned int *) data, size),
+        ROCRAND_STATUS_SUCCESS
+    );
+
+    EXPECT_EQ(hipFree(data), hipSuccess);
+    EXPECT_EQ(
+        rocrand_destroy_generator(generator),
+        ROCRAND_STATUS_SUCCESS
+    );
+}
+
+TEST(rocrand_generate_tests, simple_neg_test)
+{
+    const size_t size = 256;
+    unsigned int * data = NULL;
+
+    rocrand_generator generator = NULL;
+    EXPECT_EQ(
+        rocrand_generate(generator, (unsigned int *) data, size),
+        ROCRAND_STATUS_NOT_INITIALIZED
+    );
+}
