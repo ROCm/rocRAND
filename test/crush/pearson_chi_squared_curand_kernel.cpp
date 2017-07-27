b21b6f17c600b5518773cd86af824f9a6a19038a
Anton Gorenko <anton@streamcomputing.eu>
2017-07-27 17:50:58 +0600

    Implement Pearson tests of kernel functions
    [ROCm/rocRAND commit: eceda5dcd140628d8e64f8894f4ec83101780559]

--- test/crush/pearson_chi_squared_curand_kernel.cpp
+++ test/crush/pearson_chi_squared_curand_kernel.cpp
@@ -32,6 +32,7 @@
 
 #include <cuda_runtime.h>
 #include <curand.h>
+#include <curand_kernel.h>
 
 #include "pearson_chi_squared_common.hpp"
 
@@ -42,43 +43,139 @@ extern "C" {
 #include "finv.h"
 }
 
-#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return exit(EXIT_FAILURE);}} while(0)
+#define CUDA_CALL(x) do { \
+    cudaError_t error = (x);\
+    if(error!=cudaSuccess) { \
+    printf("Error %d at %s:%d\n",error,__FILE__,__LINE__);\
+    exit(EXIT_FAILURE);}} while(0)
 #define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return exit(EXIT_FAILURE);}} while(0)
+    exit(EXIT_FAILURE);}} while(0)
 
-typedef curandRngType rng_type_t;
+template<typename GeneratorState>
+__global__
+void init_kernel(GeneratorState * states,
+                 const unsigned long long seed,
+                 const unsigned long long offset)
+{
+    const unsigned int state_id = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int subsequence = state_id;
+    GeneratorState state;
+    curand_init(seed, subsequence, offset, &state);
+    states[state_id] = state;
+}
 
-template<typename T>
-using generate_func_type = std::function<curandStatus_t(curandGenerator_t, T *, size_t)>;
+template<typename GeneratorState>
+struct initializer
+{
+    void operator()(const size_t blocks,
+                    const size_t threads,
+                    GeneratorState * states,
+                    const unsigned long long seed,
+                    const unsigned long long offset)
+    {
+        init_kernel<<<blocks, threads>>>(states, seed, offset);
+    }
+};
 
-template<typename T>
+template<typename GeneratorState, typename Directions>
+__global__
+void init_kernel_sobol(GeneratorState * states,
+                       const Directions directions,
+                       const unsigned long long offset)
+{
+    const unsigned int state_id = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int subsequence = state_id;
+    GeneratorState state;
+    curand_init(&directions[subsequence % 20000], offset, &state);
+    states[state_id] = state;
+}
+
+template<>
+struct initializer<curandStateSobol32_t>
+{
+    initializer()
+    {
+        const size_t size = 20000 * sizeof(curandDirectionVectors32_t);
+        CUDA_CALL(cudaMalloc((void **)&directions, size));
+        curandDirectionVectors32_t * h_directions;
+        CURAND_CALL(curandGetDirectionVectors32(&h_directions, CURAND_DIRECTION_VECTORS_32_JOEKUO6));
+        CUDA_CALL(cudaMemcpy(directions, h_directions, size, cudaMemcpyHostToDevice));
+    }
+
+    ~initializer()
+    {
+        CUDA_CALL(cudaFree(directions));
+    }
+
+    void operator()(const size_t blocks,
+                    const size_t threads,
+                    curandStateSobol32_t * states,
+                    const unsigned long long seed,
+                    const unsigned long long offset)
+    {
+        init_kernel_sobol<<<blocks, threads>>>(states, directions, offset);
+    }
+
+    unsigned int * directions;
+};
+
+template<typename T, typename GeneratorState, typename GenerateFunc, typename Extra>
+__global__
+void generate_kernel(GeneratorState * states,
+                     T * data,
+                     const size_t size,
+                     const GenerateFunc& generate_func,
+                     const Extra extra)
+{
+    const unsigned int state_id = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int stride = gridDim.x * blockDim.x;
+
+    GeneratorState state = states[state_id];
+    unsigned int index = state_id;
+    while(index < size)
+    {
+        data[index] = generate_func(&state, extra);
+        index += stride;
+    }
+    states[state_id] = state;
+}
+
+template<typename T, typename GeneratorState, typename GenerateFunc, typename Extra>
 void run_test(const boost::program_options::variables_map& vm,
-              const rng_type_t rng_type,
               const std::string plot_name,
-              generate_func_type<T> generate_func,
+              const GenerateFunc& generate_func,
+              const Extra extra,
               const double mean, const double stddev,
-              distribution_func_type distribution_func)
+              const distribution_func_type& distribution_func)
 {
     const size_t size = vm["size"].as<size_t>();
     const size_t trials = vm["trials"].as<size_t>();
     const bool save_plots = vm.count("plots");
 
+    const size_t blocks = vm["blocks"].as<size_t>();
+    const size_t threads = vm["threads"].as<size_t>();
+
     T * data;
     CUDA_CALL(cudaMalloc((void **)&data, size * trials * sizeof(T)));
 
-    curandGenerator_t generator;
-    CURAND_CALL(curandCreateGenerator(&generator, rng_type));
+    const size_t states_size = blocks * threads;
+    GeneratorState * states;
+    CUDA_CALL(cudaMalloc((void **)&states, states_size * sizeof(GeneratorState)));
 
-    CURAND_CALL(generate_func(generator, data, size * trials));
+    initializer<GeneratorState> init;
+    init(blocks, threads, states, 12345ULL, 6789ULL);
+    CUDA_CALL(cudaPeekAtLastError());
+    CUDA_CALL(cudaDeviceSynchronize());
+
+    generate_kernel<<<blocks, threads>>>(states, data, size * trials, generate_func, extra);
+    CUDA_CALL(cudaPeekAtLastError());
     CUDA_CALL(cudaDeviceSynchronize());
 
     std::vector<T> h_data(size * trials);
     CUDA_CALL(cudaMemcpy(h_data.data(), data, size * trials * sizeof(T), cudaMemcpyDeviceToHost));
 
-    CURAND_CALL(curandDestroyGenerator(generator));
+    CUDA_CALL(cudaFree(states));
     CUDA_CALL(cudaFree(data));
 
     analyze(size, trials, h_data.data(),
@@ -86,67 +183,67 @@ void run_test(const boost::program_options::variables_map& vm,
             mean, stddev, distribution_func);
 }
 
+template<typename GeneratorState>
 void run_tests(const boost::program_options::variables_map& vm,
-               const rng_type_t rng_type,
                const std::string& distribution,
                const std::string plot_name)
 {
     if (distribution == "uniform-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, float * data, size_t size) {
-                return curandGenerateUniform(gen, data, size);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_uniform(state);
+            }, 0,
             0.5, std::sqrt(1.0 / 12.0),
             [](double x) { return fdist_Unif(x); }
         );
     }
     if (distribution == "uniform-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, double * data, size_t size) {
-                return curandGenerateUniformDouble(gen, data, size);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_uniform_double(state);
+            }, 0,
             0.5, std::sqrt(1.0 / 12.0),
             [](double x) { return fdist_Unif(x); }
         );
     }
     if (distribution == "normal-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, float * data, size_t size) {
-                return curandGenerateNormal(gen, data, size, 0.0f, 1.0f);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_normal(state);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_Normal2(x); }
         );
     }
     if (distribution == "normal-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, double * data, size_t size) {
-                return curandGenerateNormalDouble(gen, data, size, 0.0, 1.0);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_normal_double(state);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_Normal2(x); }
         );
     }
     if (distribution == "log-normal-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, float * data, size_t size) {
-                return curandGenerateLogNormal(gen, data, size, 0.0f, 1.0f);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_log_normal(state, 0.0f, 1.0f);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_LogNormal(0.0, 1.0, x); }
         );
     }
     if (distribution == "log-normal-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](curandGenerator_t gen, double * data, size_t size) {
-                return curandGenerateLogNormalDouble(gen, data, size, 0.0, 1.0);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return curand_log_normal_double(state, 0.0, 1.0);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_LogNormal(0.0, 1.0, x); }
         );
@@ -158,10 +255,10 @@ void run_tests(const boost::program_options::variables_map& vm,
         {
             std::cout << "    " << "lambda "
                  << std::fixed << std::setprecision(1) << lambda << std::endl;
-            run_test<unsigned int>(vm, rng_type, plot_name,
-                [lambda](curandGenerator_t gen, unsigned int * data, size_t size) {
-                    return curandGeneratePoisson(gen, data, size, lambda);
-                },
+            run_test<unsigned int, GeneratorState>(vm, plot_name,
+                [] __device__ (GeneratorState * state, double lambda) {
+                    return curand_poisson(state, lambda);
+                }, lambda,
                 lambda, std::sqrt(lambda),
                 [lambda](double x) { return fdist_Poisson1(lambda, static_cast<long>(std::round(x)) - 1); }
             );
@@ -169,16 +266,16 @@ void run_tests(const boost::program_options::variables_map& vm,
     }
 }
 
-const std::vector<std::pair<rng_type_t, std::string>> all_engines = {
-    { CURAND_RNG_PSEUDO_XORWOW, "xorwow" },
-    { CURAND_RNG_PSEUDO_MRG32K3A, "mrg32k3a" },
-    { CURAND_RNG_PSEUDO_MTGP32, "mtgp32" },
-    { CURAND_RNG_PSEUDO_MT19937, "mt19937" },
-    { CURAND_RNG_PSEUDO_PHILOX4_32_10, "philox" },
-    { CURAND_RNG_QUASI_SOBOL32, "sobol32" },
-    { CURAND_RNG_QUASI_SCRAMBLED_SOBOL32, "scrambled_sobol32" },
-    { CURAND_RNG_QUASI_SOBOL64, "sobol64" },
-    { CURAND_RNG_QUASI_SCRAMBLED_SOBOL64, "scrambled_sobol64" }
+const std::vector<std::string> all_engines = {
+    "xorwow",
+    "mrg32k3a",
+    // "mtgp32",
+    // "mt19937",
+    "philox",
+    "sobol32",
+    // "scrambled_sobol32",
+    // "sobol64",
+    // "scrambled_sobol64",
 };
 
 const std::vector<std::string> all_distributions = {
@@ -207,8 +304,8 @@ int main(int argc, char *argv[])
     const std::string engine_desc =
         "space-separated list of random number engines:" +
         std::accumulate(all_engines.begin(), all_engines.end(), std::string(),
-            [](std::string a, std::pair<rng_type_t, std::string> b) {
-                return a + "\n   " + b.second;
+            [](std::string a, std::string b) {
+                return a + "\n   " + b;
             }
         ) +
         "\nor all";
@@ -216,6 +313,8 @@ int main(int argc, char *argv[])
         ("help", "show usage instructions")
         ("size", po::value<size_t>()->default_value(10000), "number of values")
         ("trials", po::value<size_t>()->default_value(20), "number of trials")
+        ("blocks", po::value<size_t>()->default_value(64), "number of blocks")
+        ("threads", po::value<size_t>()->default_value(256), "number of threads in each block")
         ("dis", po::value<std::vector<std::string>>()->multitoken()->default_value({ "all" }, "all"),
             distribution_desc.c_str())
         ("engine", po::value<std::vector<std::string>>()->multitoken()->default_value({ "philox" }, "philox"),
@@ -234,7 +333,7 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    std::vector<std::pair<rng_type_t, std::string>> engines;
+    std::vector<std::string> engines;
     {
         auto es = vm["engine"].as<std::vector<std::string>>();
         if (std::find(es.begin(), es.end(), "all") != es.end())
@@ -245,7 +344,7 @@ int main(int argc, char *argv[])
         {
             for (auto e : all_engines)
             {
-                if (std::find(es.begin(), es.end(), e.second) != es.end())
+                if (std::find(es.begin(), es.end(), e) != es.end())
                     engines.push_back(e);
             }
         }
@@ -269,16 +368,29 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "cuRAND:" << std::endl << std::endl;
-    for (auto e : engines)
+    for (auto engine : engines)
     {
-        const std::string engine_name = e.second;
-        std::cout << engine_name << ":" << std::endl;
-        const rng_type_t rng_type = e.first;
+        std::cout << engine << ":" << std::endl;
         for (auto distribution : distributions)
         {
             std::cout << "  " << distribution << ":" << std::endl;
-            const std::string plot_name = engine_name + "-" + distribution;
-            run_tests(vm, rng_type, distribution, plot_name);
+            const std::string plot_name = engine + "-" + distribution;
+            if (engine == "xorwow")
+            {
+                run_tests<curandStateXORWOW_t>(vm, distribution, plot_name);
+            }
+            else if (engine == "mrg32k3a")
+            {
+                run_tests<curandStateMRG32k3a_t>(vm, distribution, plot_name);
+            }
+            else if (engine == "philox")
+            {
+                run_tests<curandStatePhilox4_32_10_t>(vm, distribution, plot_name);
+            }
+            else if (engine == "sobol32")
+            {
+                run_tests<curandStateSobol32_t>(vm, distribution, plot_name);
+            }
         }
     }
 
