eceda5dcd140628d8e64f8894f4ec83101780559
Anton Gorenko <anton@streamcomputing.eu>
2017-07-27 17:50:58 +0600

    Implement Pearson tests of kernel functions

--- test/crush/pearson_chi_squared_rocrand_kernel.cpp
+++ test/crush/pearson_chi_squared_rocrand_kernel.cpp
@@ -32,6 +32,7 @@
 
 #include <hip/hip_runtime.h>
 #include <rocrand.h>
+#include <rocrand_kernel.h>
 
 #include "pearson_chi_squared_common.hpp"
 
@@ -60,36 +61,109 @@ extern "C" {
     } \
   }
 
-typedef rocrand_rng_type rng_type_t;
+template<typename GeneratorState>
+__global__
+void init_kernel(GeneratorState * states,
+                 const unsigned long long seed,
+                 const unsigned long long offset)
+{
+    const unsigned int state_id = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+    const unsigned int subsequence = state_id;
+    GeneratorState state;
+    rocrand_init(seed, subsequence, offset, &state);
+    states[state_id] = state;
+}
 
-template<typename T>
-using generate_func_type = std::function<rocrand_status(rocrand_generator, T *, size_t)>;
+template<typename GeneratorState>
+struct initializer
+{
+    void operator()(const size_t blocks,
+                    const size_t threads,
+                    GeneratorState * states,
+                    const unsigned long long seed,
+                    const unsigned long long offset)
+    {
+        hipLaunchKernelGGL(
+            HIP_KERNEL_NAME(init_kernel),
+            dim3(blocks), dim3(threads), 0, 0,
+            states, seed, offset
+        );
+    }
+};
 
-template<typename T>
+template<typename GeneratorState, typename Directions>
+__global__
+void init_kernel_sobol(GeneratorState * states,
+                       const Directions directions,
+                       const unsigned long long offset)
+{
+    const unsigned int state_id = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+    const unsigned int subsequence = state_id;
+    GeneratorState state;
+    rocrand_init(&directions[subsequence % 20000], offset, &state);
+    states[state_id] = state;
+}
+
+template<typename T, typename GeneratorState, typename GenerateFunc, typename Extra>
+__global__
+void generate_kernel(GeneratorState * states,
+                     T * data,
+                     const size_t size,
+                     const GenerateFunc& generate_func,
+                     const Extra extra)
+{
+    const unsigned int state_id = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+    const unsigned int stride = hipGridDim_x * hipBlockDim_x;
+
+    GeneratorState state = states[state_id];
+    unsigned int index = state_id;
+    while(index < size)
+    {
+        data[index] = generate_func(&state, extra);
+        index += stride;
+    }
+    states[state_id] = state;
+}
+
+template<typename T, typename GeneratorState, typename GenerateFunc, typename Extra>
 void run_test(const boost::program_options::variables_map& vm,
-              const rng_type_t rng_type,
               const std::string plot_name,
-              generate_func_type<T> generate_func,
+              const GenerateFunc& generate_func,
+              const Extra extra,
               const double mean, const double stddev,
-              distribution_func_type distribution_func)
+              const distribution_func_type& distribution_func)
 {
     const size_t size = vm["size"].as<size_t>();
     const size_t trials = vm["trials"].as<size_t>();
     const bool save_plots = vm.count("plots");
 
+    const size_t blocks = vm["blocks"].as<size_t>();
+    const size_t threads = vm["threads"].as<size_t>();
+
     T * data;
     HIP_CHECK(hipMalloc((void **)&data, size * trials * sizeof(T)));
 
-    rocrand_generator generator;
-    ROCRAND_CHECK(rocrand_create_generator(&generator, rng_type));
+    const size_t states_size = blocks * threads;
+    GeneratorState * states;
+    HIP_CHECK(hipMalloc((void **)&states, states_size * sizeof(GeneratorState)));
 
-    ROCRAND_CHECK(generate_func(generator, data, size * trials));
+    initializer<GeneratorState> init;
+    init(blocks, threads, states, 12345ULL, 6789ULL);
+    HIP_CHECK(hipPeekAtLastError());
+    HIP_CHECK(hipDeviceSynchronize());
+
+    hipLaunchKernelGGL(
+        HIP_KERNEL_NAME(generate_kernel),
+        dim3(blocks), dim3(threads), 0, 0,
+        states, data, size * trials, generate_func, extra
+    );
+    HIP_CHECK(hipPeekAtLastError());
     HIP_CHECK(hipDeviceSynchronize());
 
     std::vector<T> h_data(size * trials);
     HIP_CHECK(hipMemcpy(h_data.data(), data, size * trials * sizeof(T), hipMemcpyDeviceToHost));
 
-    ROCRAND_CHECK(rocrand_destroy_generator(generator));
+    HIP_CHECK(hipFree(states));
     HIP_CHECK(hipFree(data));
 
     analyze(size, trials, h_data.data(),
@@ -97,67 +171,67 @@ void run_test(const boost::program_options::variables_map& vm,
             mean, stddev, distribution_func);
 }
 
+template<typename GeneratorState>
 void run_tests(const boost::program_options::variables_map& vm,
-               const rng_type_t rng_type,
                const std::string& distribution,
                const std::string plot_name)
 {
     if (distribution == "uniform-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, float * data, size_t size) {
-                return rocrand_generate_uniform(gen, data, size);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_uniform(state);
+            }, 0,
             0.5, std::sqrt(1.0 / 12.0),
             [](double x) { return fdist_Unif(x); }
         );
     }
     if (distribution == "uniform-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, double * data, size_t size) {
-                return rocrand_generate_uniform_double(gen, data, size);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_uniform_double(state);
+            }, 0,
             0.5, std::sqrt(1.0 / 12.0),
             [](double x) { return fdist_Unif(x); }
         );
     }
     if (distribution == "normal-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, float * data, size_t size) {
-                return rocrand_generate_normal(gen, data, size, 0.0f, 1.0f);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_normal(state);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_Normal2(x); }
         );
     }
     if (distribution == "normal-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, double * data, size_t size) {
-                return rocrand_generate_normal_double(gen, data, size, 0.0, 1.0);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_normal_double(state);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_Normal2(x); }
         );
     }
     if (distribution == "log-normal-float")
     {
-        run_test<float>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, float * data, size_t size) {
-                return rocrand_generate_log_normal(gen, data, size, 0.0f, 1.0f);
-            },
+        run_test<float, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_log_normal(state, 0.0f, 1.0f);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_LogNormal(0.0, 1.0, x); }
         );
     }
     if (distribution == "log-normal-double")
     {
-        run_test<double>(vm, rng_type, plot_name,
-            [](rocrand_generator gen, double * data, size_t size) {
-                return rocrand_generate_log_normal_double(gen, data, size, 0.0, 1.0);
-            },
+        run_test<double, GeneratorState>(vm, plot_name,
+            [] __device__ (GeneratorState * state, int) {
+                return rocrand_log_normal_double(state, 0.0, 1.0);
+            }, 0,
             0.0, 1.0,
             [](double x) { return fdist_LogNormal(0.0, 1.0, x); }
         );
@@ -169,10 +243,10 @@ void run_tests(const boost::program_options::variables_map& vm,
         {
             std::cout << "    " << "lambda "
                  << std::fixed << std::setprecision(1) << lambda << std::endl;
-            run_test<unsigned int>(vm, rng_type, plot_name,
-                [lambda](rocrand_generator gen, unsigned int * data, size_t size) {
-                    return rocrand_generate_poisson(gen, data, size, lambda);
-                },
+            run_test<unsigned int, GeneratorState>(vm, plot_name,
+                [] __device__ (GeneratorState * state, double lambda) {
+                    return rocrand_poisson(state, lambda);
+                }, lambda,
                 lambda, std::sqrt(lambda),
                 [lambda](double x) { return fdist_Poisson1(lambda, static_cast<long>(std::round(x)) - 1); }
             );
@@ -180,12 +254,16 @@ void run_tests(const boost::program_options::variables_map& vm,
     }
 }
 
-const std::vector<std::pair<rng_type_t, std::string>> all_engines = {
-    { ROCRAND_RNG_PSEUDO_XORWOW, "xorwow" },
-    { ROCRAND_RNG_PSEUDO_MRG32K3A, "mrg32k3a" },
-    // { ROCRAND_RNG_PSEUDO_MTGP32, "mtgp32" },
-    { ROCRAND_RNG_PSEUDO_PHILOX4_32_10, "philox" },
-    // { ROCRAND_RNG_QUASI_SOBOL32, "sobol32" },
+const std::vector<std::string> all_engines = {
+    "xorwow",
+    "mrg32k3a",
+    // "mtgp32",
+    // "mt19937",
+    "philox",
+    // "sobol32",
+    // "scrambled_sobol32",
+    // "sobol64",
+    // "scrambled_sobol64",
 };
 
 const std::vector<std::string> all_distributions = {
@@ -214,8 +292,8 @@ int main(int argc, char *argv[])
     const std::string engine_desc =
         "space-separated list of random number engines:" +
         std::accumulate(all_engines.begin(), all_engines.end(), std::string(),
-            [](std::string a, std::pair<rng_type_t, std::string> b) {
-                return a + "\n   " + b.second;
+            [](std::string a, std::string b) {
+                return a + "\n   " + b;
             }
         ) +
         "\nor all";
@@ -223,6 +301,8 @@ int main(int argc, char *argv[])
         ("help", "show usage instructions")
         ("size", po::value<size_t>()->default_value(10000), "number of values")
         ("trials", po::value<size_t>()->default_value(20), "number of trials")
+        ("blocks", po::value<size_t>()->default_value(64), "number of blocks")
+        ("threads", po::value<size_t>()->default_value(256), "number of threads in each block")
         ("dis", po::value<std::vector<std::string>>()->multitoken()->default_value({ "all" }, "all"),
             distribution_desc.c_str())
         ("engine", po::value<std::vector<std::string>>()->multitoken()->default_value({ "philox" }, "philox"),
@@ -241,7 +321,7 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    std::vector<std::pair<rng_type_t, std::string>> engines;
+    std::vector<std::string> engines;
     {
         auto es = vm["engine"].as<std::vector<std::string>>();
         if (std::find(es.begin(), es.end(), "all") != es.end())
@@ -252,7 +332,7 @@ int main(int argc, char *argv[])
         {
             for (auto e : all_engines)
             {
-                if (std::find(es.begin(), es.end(), e.second) != es.end())
+                if (std::find(es.begin(), es.end(), e) != es.end())
                     engines.push_back(e);
             }
         }
@@ -276,16 +356,25 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "rocRAND:" << std::endl << std::endl;
-    for (auto e : engines)
+    for (auto engine : engines)
     {
-        const std::string engine_name = e.second;
-        std::cout << engine_name << ":" << std::endl;
-        const rng_type_t rng_type = e.first;
+        std::cout << engine << ":" << std::endl;
         for (auto distribution : distributions)
         {
             std::cout << "  " << distribution << ":" << std::endl;
-            const std::string plot_name = engine_name + "-" + distribution;
-            run_tests(vm, rng_type, distribution, plot_name);
+            const std::string plot_name = engine + "-" + distribution;
+            if (engine == "xorwow")
+            {
+                run_tests<rocrand_state_xorwow>(vm, distribution, plot_name);
+            }
+            else if (engine == "mrg32k3a")
+            {
+                run_tests<rocrand_state_mrg32k3a>(vm, distribution, plot_name);
+            }
+            else if (engine == "philox")
+            {
+                run_tests<rocrand_state_philox4x32_10>(vm, distribution, plot_name);
+            }
         }
     }
 
