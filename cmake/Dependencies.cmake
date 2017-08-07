b200a33ecbb8598107b4a7cedd09b2020568164e
Jakub Szuppe <jakub@streamcomputing.eu>
2017-08-07 20:18:27 +0200

    Download/build Boost on cmake config
    [ROCm/rocRAND commit: a1b074d4e85cb2ac02509c4af8ddf8c2579e5647]

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -45,61 +45,42 @@ endif()
 # Boost.Program_options is required only for benchmarks and crush tests
 if(BUILD_BENCHMARK OR BUILD_CRUSH_TEST)
     set(ROCRAND_BOOST_COMPONENTS program_options)
-    find_package(Boost 1.54 QUIET COMPONENTS ${ROCRAND_BOOST_COMPONENTS})
+    if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+        find_package(Boost 1.54 QUIET COMPONENTS ${ROCRAND_BOOST_COMPONENTS})
+    endif()
 
     # Download, build, and install Boost if not found
     if(NOT Boost_FOUND)
-        include(ExternalProject)
-        include(FindPackageHandleStandardArgs)
-
-        set(Boost_FIND_COMPONENTS ${ROCRAND_BOOST_COMPONENTS})
-        set(BOOST_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/boost)
-
-        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-            set(boost_am 64)
-        else()
-            set(boost_am 32)
-        endif()
-        mark_as_advanced(boost_am)
-
-        set(boost_variant "release")
-        if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-            set(boost_variant "debug")
-        endif()
-        mark_as_advanced(boost_variant)
-
-        foreach(component ${Boost_FIND_COMPONENTS})
-            list(APPEND Boost_COMPONENTS_FOR_BUILD --with-${component})
-        endforeach()
-
-        ExternalProject_Add(boost
-            URL                 https://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.bz2/download
-            URL_MD5             93eecce2abed9d2442c9676914709349
-            UPDATE_DISCONNECTED TRUE
-            CONFIGURE_COMMAND   ./bootstrap.sh --prefix=${BOOST_ROOT_DIR}
-            BUILD_COMMAND       ./b2 --prefix=${BOOST_ROOT_DIR} -d0 address-model=${boostlib_am} variant=${boost_variant} ${Boost_COMPONENTS_FOR_BUILD} install
-            INSTALL_COMMAND     ""
-            INSTALL_DIR         ${BOOST_ROOT_DIR}
-            BUILD_IN_SOURCE     TRUE
+        message(STATUS "Boost not found. Downloading and building Boost.")
+        set(BOOST_ROOT ${CMAKE_BINARY_DIR}/boost CACHE PATH "")
+        configure_file(
+            ${CMAKE_CURRENT_LIST_DIR}/DownloadBoost.CMakeLists.txt.in boost-download/CMakeLists.txt
+            @ONLY
         )
-        ExternalProject_Get_Property(boost install_dir)
-        set(BOOST_ROOT ${install_dir} CACHE PATH "")
-        set(Boost_DIR ${install_dir} CACHE PATH "")
-        set(Boost_INCLUDE_DIRS ${install_dir}/include)
+        # Configure
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" -D "CMAKE_MAKE_PROGRAM:FILE=${CMAKE_MAKE_PROGRAM}" .
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost-download
+            RESULT_VARIABLE result
+        )
+        if(result)
+            message(FATAL_ERROR "CMake/config step for Boost failed: ${result}")
+            unset(BOOST_ROOT)
+        endif()
+        # Build and install
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} --build .
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost-download
+            RESULT_VARIABLE result
+        )
+        if(result)
+            message(FATAL_ERROR "Build step for Boost failed: ${result}")
+            unset(BOOST_ROOT)
+        endif()
 
-        set(Boost_LIBRARIES)
-        foreach(component ${Boost_FIND_COMPONENTS})
-            list(APPEND Boost_LIBRARIES ${BOOST_ROOT_DIR}/lib/${LIBRARY_PREFIX}boost_${component}${LIBRARY_SUFFIX})
-        endforeach()
-
-        FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-            Boost DEFAULT_MSG
-		    Boost_INCLUDE_DIRS Boost_LIBRARIES
-		)
-        mark_as_advanced(Boost_INCLUDE_DIRS Boost_LIBRARIES)
-
-        # Add dependency
-        list(APPEND ROCRAND_BENCHMARK_DEPENDENCIES boost)
-        list(APPEND ROCRAND_CRUSH_TEST_DEPENDENCIES boost)
+        find_package(Boost 1.54
+            REQUIRED
+            COMPONENTS ${ROCRAND_BOOST_COMPONENTS}
+        )
     endif()
 endif()
