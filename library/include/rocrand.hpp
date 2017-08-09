8e69cf24731019ce4454816599676b0b986bc0b0
Jakub Szuppe <jakub@streamcomputing.eu>
2017-08-09 18:12:09 +0200

    rocRAND C++ Wrapper basics
    [ROCm/rocRAND commit: 2003277a50331883031cf0cd22f567905898894b]

---
+++ library/include/rocrand.hpp
@@ -0,0 +1,228 @@
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef ROCRAND_HPP_
+#define ROCRAND_HPP_
+
+// At least C++11 required
+#if defined(__cplusplus) && __cplusplus >= 201103L
+
+#include <random>
+#include <exception>
+#include <string>
+#include <sstream>
+#include <type_traits>
+
+#include "rocrand.h"
+#include "rocrand_kernel.h"
+
+namespace rocrand_cpp {
+
+/// \class error
+/// \brief A run-time rocRAND error.
+///
+/// The error class represents an error returned from an rocRAND
+/// function.
+///
+/// \see context_error
+class error : public std::exception
+{
+public:
+    typedef rocrand_status error_type;
+
+    error(error_type error) noexcept
+        : m_error(error),
+          m_error_string(to_string(error))
+    {
+
+    }
+
+    ~error() noexcept
+    {
+    }
+
+    /// Returns the numeric error code.
+    error_type error_code() const noexcept
+    {
+        return m_error;
+    }
+
+    /// Returns a string description of the error.
+    std::string error_string() const noexcept
+    {
+        return m_error_string;
+    }
+
+    /// Returns a C-string description of the error.
+    const char* what() const noexcept
+    {
+        return m_error_string.c_str();
+    }
+
+    static std::string to_string(error_type error)
+    {
+        switch(error)
+        {
+            // TODO
+            default: {
+                std::stringstream s;
+                s << "Unknown rocRAND Error (" << error << ")";
+                return s.str();
+            }
+        }
+    }
+
+private:
+    error_type m_error;
+    std::string m_error_string;
+};
+
+namespace detail {
+
+template<rocrand_rng_type GeneratorType>
+class rng_engine
+{
+public:
+    typedef unsigned int result_type; // 32bit uint
+
+    rng_engine()
+    {
+        rocrand_status status;
+        status = rocrand_create_generator(&m_generator, GeneratorType);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+
+    ~rng_engine()
+    {
+        rocrand_status status = rocrand_destroy_generator(m_generator);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+
+    void stream(hipStream_t value)
+    {
+        rocrand_status status = rocrand_set_stream(m_generator, value);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+
+protected:
+    rocrand_generator m_generator;
+};
+
+template<rocrand_rng_type GeneratorType, unsigned long long DefaultSeed>
+class prng_engine : public rng_engine<GeneratorType>
+{
+    typedef rng_engine<GeneratorType> base_type;
+
+public:
+
+    typedef unsigned long long seed_type; // 64bit uint
+    typedef unsigned long long offset_type;
+    typedef typename base_type::result_type result_type;
+
+    prng_engine(seed_type seed_value = DefaultSeed,
+                offset_type offset_value = 0)
+        : base_type()
+    {
+        this->seed(seed_value);
+        if(offset_value > 0)
+        {
+            this->offset(offset_value);
+        }
+    }
+
+    ~prng_engine()
+    {
+    }
+
+    void seed(seed_type value)
+    {
+        rocrand_status status = rocrand_set_seed(this->m_generator, value);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+
+    void offset(offset_type value)
+    {
+        rocrand_status status = rocrand_set_offset(this->m_generator, value);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+};
+
+} // end detail namespace
+
+template<class IntType = unsigned int>
+class uniform_int_distribution
+{
+public:
+    typedef IntType result_type;
+
+    uniform_int_distribution()
+    {
+        static_assert(
+            std::is_same<IntType, unsigned int>::value,
+             "Only unsigned int type is supported in uniform_int_distribution"
+        );
+    }
+
+    void reset()
+    {
+    }
+
+    template<class Generator>
+    void operator()(Generator& g, IntType * output, size_t size)
+    {
+        rocrand_status status = rocrand_generate(g.m_generator, output, size);
+        if(status != ROCRAND_STATUS_SUCCESS) throw rocrand_cpp::error(status);
+    }
+};
+
+class philox4x32_10_engine : public detail::prng_engine<ROCRAND_RNG_PSEUDO_PHILOX4_32_10, ROCRAND_PHILOX4x32_DEFAULT_SEED>
+{
+    typedef detail::prng_engine<ROCRAND_RNG_PSEUDO_PHILOX4_32_10, ROCRAND_PHILOX4x32_DEFAULT_SEED> base_type;
+public:
+    using base_type::base_type;
+
+    template<class T>
+    friend class ::rocrand_cpp::uniform_int_distribution;
+};
+
+class xorwow_engine : public detail::prng_engine<ROCRAND_RNG_PSEUDO_XORWOW, ROCRAND_XORWOW_DEFAULT_SEED>
+{
+    typedef detail::prng_engine<ROCRAND_RNG_PSEUDO_XORWOW, ROCRAND_XORWOW_DEFAULT_SEED> base_type;
+public:
+    using base_type::base_type;
+
+    template<class T>
+    friend class ::rocrand_cpp::uniform_int_distribution;
+};
+
+class mrg32k3a_engine : public detail::prng_engine<ROCRAND_RNG_PSEUDO_MRG32K3A, ROCRAND_MRG32K3A_DEFAULT_SEED>
+{
+    typedef detail::prng_engine<ROCRAND_RNG_PSEUDO_MRG32K3A, ROCRAND_MRG32K3A_DEFAULT_SEED> base_type;
+public:
+    using base_type::base_type;
+
+    template<class T>
+    friend class ::rocrand_cpp::uniform_int_distribution;
+};
+
+} // end namespace rocrand
+
+#endif // #if __cplusplus >= 201103L
+#endif // ROCRAND_HPP_
---
+++ test/test_rocrand_cpp_wrapper.cpp
@@ -0,0 +1,141 @@
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+#include <hip/hip_runtime.h>
+#include <rocrand.hpp>
+
+#define HIP_CHECK(x) ASSERT_EQ(x, hipSuccess)
+
+template<class T>
+void rocrand_prng_ctor_template()
+{
+    T();
+    T(11ULL); // seed
+    T(11ULL, 2ULL); // seed, offset
+}
+
+TEST(rocrand_cpp_wrapper, rocrand_prng_ctor)
+{
+    ASSERT_NO_THROW(rocrand_prng_ctor_template<rocrand_cpp::philox4x32_10_engine>());
+    ASSERT_NO_THROW(rocrand_prng_ctor_template<rocrand_cpp::xorwow_engine>());
+    ASSERT_NO_THROW(rocrand_prng_ctor_template<rocrand_cpp::mrg32k3a_engine>());
+}
+
+template<class T>
+void rocrand_prng_seed_template()
+{
+    T engine;
+    engine.seed(11ULL);
+    engine.seed(12ULL);
+}
+
+TEST(rocrand_cpp_wrapper, rocrand_prng_seed)
+{
+    ASSERT_NO_THROW(rocrand_prng_seed_template<rocrand_cpp::philox4x32_10_engine>());
+    ASSERT_NO_THROW(rocrand_prng_seed_template<rocrand_cpp::xorwow_engine>());
+    ASSERT_NO_THROW(rocrand_prng_seed_template<rocrand_cpp::mrg32k3a_engine>());
+}
+
+template<class T>
+void rocrand_prng_offset_template()
+{
+    T engine;
+    engine.offset(11ULL);
+    engine.offset(12ULL);
+}
+
+TEST(rocrand_cpp_wrapper, rocrand_prng_offset)
+{
+    ASSERT_NO_THROW(rocrand_prng_offset_template<rocrand_cpp::philox4x32_10_engine>());
+    ASSERT_NO_THROW(rocrand_prng_offset_template<rocrand_cpp::xorwow_engine>());
+    ASSERT_NO_THROW(rocrand_prng_offset_template<rocrand_cpp::mrg32k3a_engine>());
+}
+
+template<class T>
+void rocrand_rng_stream_template()
+{
+    T engine;
+    hipStream_t stream;
+    ASSERT_EQ(hipStreamCreate(&stream), hipSuccess);
+    engine.stream(stream);
+    engine.stream(NULL);
+}
+
+TEST(rocrand_cpp_wrapper, rocrand_rng_stream)
+{
+    ASSERT_NO_THROW(rocrand_rng_stream_template<rocrand_cpp::philox4x32_10_engine>());
+    ASSERT_NO_THROW(rocrand_rng_stream_template<rocrand_cpp::xorwow_engine>());
+    ASSERT_NO_THROW(rocrand_rng_stream_template<rocrand_cpp::mrg32k3a_engine>());
+}
+
+template<class T, class IntType>
+void rocrand_uniform_int_dist_template()
+{
+    T engine;
+    rocrand_cpp::uniform_int_distribution<> d;
+
+    const size_t output_size = 8192;
+    IntType * output;
+    ASSERT_EQ(
+        hipMalloc((void **)&output,
+        output_size * sizeof(IntType)),
+        hipSuccess
+    );
+    ASSERT_EQ(hipDeviceSynchronize(), hipSuccess);
+
+    // generate
+    EXPECT_NO_THROW(d(engine, output, output_size));
+    HIP_CHECK(hipDeviceSynchronize());
+
+    std::vector<unsigned int> output_host(output_size);
+    HIP_CHECK(
+        hipMemcpy(
+            output_host.data(), output,
+            output_size * sizeof(unsigned int),
+            hipMemcpyDeviceToHost
+        )
+    );
+    HIP_CHECK(hipDeviceSynchronize());
+    HIP_CHECK(hipFree(output));
+
+    double mean = 0;
+    for(auto v : output_host)
+    {
+        mean += static_cast<double>(v) / UINT_MAX;
+    }
+    mean = mean / output_size;
+    EXPECT_NEAR(mean, 0.5, 0.1);
+}
+
+TEST(rocrand_cpp_wrapper, rocrand_uniform_int_dist)
+{
+    ASSERT_NO_THROW((
+        rocrand_uniform_int_dist_template<rocrand_cpp::philox4x32_10_engine, unsigned int>()
+    ));
+    ASSERT_NO_THROW((
+        rocrand_uniform_int_dist_template<rocrand_cpp::xorwow_engine, unsigned int>()
+    ));
+    ASSERT_NO_THROW((
+        rocrand_uniform_int_dist_template<rocrand_cpp::mrg32k3a_engine, unsigned int>()
+    ));
+}
