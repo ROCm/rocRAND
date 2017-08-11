a778a02564fa193755e4602b26d90f2676c7af7b
Jakub Szuppe <jakub@streamcomputing.eu>
2017-08-11 21:13:50 +0200

    GTest install fix

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -34,7 +34,7 @@ if(BUILD_TEST)
                          GIT_REPOSITORY      https://github.com/google/googletest.git
                          GIT_TAG             master
                          INSTALL_DIR         ${GTEST_ROOT}
-                         CMAKE_ARGS          -Dgtest_force_shared_crt=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+                         CMAKE_ARGS          -DBUILD_GTEST=ON -DINSTALL_GTEST=ON -Dgtest_force_shared_crt=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
                          LOG_DOWNLOAD        TRUE
                          LOG_CONFIGURE       TRUE
                          LOG_BUILD           TRUE
