c4bc29ec74cb8ce71185146fc3847f7029e6d1f9
Eiden Yoshida <eiden.yoshida@amd.com>
2021-04-06 10:33:52 -0600

    Use rocJenkins function to parse to add xnack to target

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -10,13 +10,15 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
     String buildTypeDir = debug ? 'debug' : 'release'
     String buildStatic = staticLibrary ? '-DBUILD_STATIC_LIBS=ON' : '-DBUILD_SHARED=OFF'
     String cmake = platform.jenkinsLabel.contains('centos') ? 'cmake3' : 'cmake'
-    String amdgpuTargets = env.BRANCH_NAME.startsWith("PR-") ? '-DAMDGPU_TARGETS=\$GFXARCH' : ''
+    //Set CI node's gfx arch as target if PR, otherwise use default targets of the library
+    String amdgpuTargets = env.BRANCH_NAME.startsWith("PR-") ? '-DAMDGPU_TARGETS=\$gfx_arch' : ''
 
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
                 mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
-                GFXARCH=\$(/opt/rocm/bin/rocm_agent_enumerator | tail -1)
+                # gfxTargetParser reads gfxarch and adds target features such as xnack
+                ${auxiliary.gfxTargetParser()}
                 ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc ${buildTypeArg} ${buildStatic} ${amdgpuTargets} -DBUILD_TEST=ON -DBUILD_BENCHMARK=ON ../..
                 make -j\$(nproc)
                 """
