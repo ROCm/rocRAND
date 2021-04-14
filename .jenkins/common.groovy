908e5e676748666a22f7da754c21d7f970b8c994
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2021-04-13 18:13:17 -0600

    Add gfx90a targets (#168)
    Co-authored-by: saadrahim <44449863+saadrahim@users.noreply.github.com>
    Co-authored-by: Cordell Bloor <Cordell.Bloor@amd.com>
    
    [ROCm/rocRAND commit: d86d46bca215397156657295c9ec44c74320aa82]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -10,12 +10,16 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
     String buildTypeDir = debug ? 'debug' : 'release'
     String buildStatic = staticLibrary ? '-DBUILD_STATIC_LIBS=ON' : '-DBUILD_SHARED=OFF'
     String cmake = platform.jenkinsLabel.contains('centos') ? 'cmake3' : 'cmake'
+    //Set CI node's gfx arch as target if PR, otherwise use default targets of the library
+    String amdgpuTargets = env.BRANCH_NAME.startsWith('PR-') ? '-DAMDGPU_TARGETS=\$gfx_arch' : ''
 
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
                 mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
-                ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc ${buildTypeArg} ${buildStatic} -DBUILD_TEST=ON -DBUILD_BENCHMARK=ON ../..
+                # gfxTargetParser reads gfxarch and adds target features such as xnack
+                ${auxiliary.gfxTargetParser()}
+                ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc ${buildTypeArg} ${buildStatic} ${amdgpuTargets} -DBUILD_TEST=ON -DBUILD_BENCHMARK=ON ../..
                 make -j\$(nproc)
                 """
     
