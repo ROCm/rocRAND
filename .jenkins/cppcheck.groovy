92a121b20f8fed46475e461e0b016bd87a6b1854
pavahora <92880691+pavahora@users.noreply.github.com>
2021-11-02 15:57:56 -0500

    Update cppcheck.groovy

--- .jenkins/cppcheck.groovy
+++ .jenkins/cppcheck.groovy
@@ -1,25 +1,53 @@
-// This file is for internal AMD use.
-
-// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+#!/usr/bin/env groovy
+@Library('rocJenkins@pong') _
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path;
 
 def runCompileCommand(platform, project, jobName, boolean debug=false, boolean staticLibrary=false)
 {
-
-project.paths.construct_build_prefix()
-
-def command = """#!/usr/bin/env bash
-
-set -x
-
-# Command to create cppcheck result xml file
-cppcheck --enable=all --inline-suppr --inconclusive --xml --xml-version=2 --output-file=cppcheck_result.xml ${project.paths.project_build_prefix}
-
-#Command to generate HTML report From the xml file
-mkdir ./reports
-cppcheck-htmlreport --title="cppcheck_Report" --report-dir=./reports --source-dir= ${project.paths.project_build_prefix} --file=cppcheck_result.xml
-"""
-platform.runCommand(this, command)
-
+	project.paths.construct_build_prefix()
+	def command = """
+	#!/usr/bin/env bash
+	set -x
+    cppcheck --enable=all --inline-suppr --inconclusive --xml --xml-version=2 --output-file=cppcheck_result.xml ${project.paths.project_build_prefix}
+    mkdir ./reports
+    cppcheck-htmlreport --title="cppcheck_Report" --report-dir=./reports --source-dir= ${project.paths.project_build_prefix} --file=cppcheck_result.xml
+    """
+    platform.runCommand(this, command)
 }
 
-return this
+ci: { 
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])]]
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900'],centos7:['gfx906'],centos8:['gfx906'],sles15sp1:['gfx908']])]
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    propertyList.each 
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    jobNameList.each
+    {
+        jobName, nodeDetails->
+        if (urlJobName == jobName)
+            stage(jobName) {
+                runCompileCommand(nodeDetails, jobName)
+            }
+    }
+
+    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
+    if(!jobNameList.keySet().contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        stage(urlJobName) {
+            runCompileCommand([ubuntu16:['gfx906']], urlJobName)
+        }
+    }
+}
