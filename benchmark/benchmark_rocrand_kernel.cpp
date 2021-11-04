740b2504fd888cbcb00f6dceba9312eefa2b29c3
AravindanC <aravindan.cheruvally@amd.com>
2021-11-04 00:03:08 -0700

    Include Path Reorg Dep Changes

--- benchmark/benchmark_rocrand_kernel.cpp
+++ benchmark/benchmark_rocrand_kernel.cpp
@@ -29,8 +29,8 @@
 #include "cmdparser.hpp"
 
 #include <hip/hip_runtime.h>
-#include <rocrand.h>
-#include <rocrand_kernel.h>
+#include <rocrand/rocrand.h>
+#include <rocrand/rocrand_kernel.h>
 #include <rocrand_mtgp32_11213.h>
 #include <rocrand_sobol32_precomputed.h>
 #include <rocrand_sobol64_precomputed.h>
