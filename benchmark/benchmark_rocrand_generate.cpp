d591e32bb80e9e0f74b36d8b7c2b1b3f56991574
Stanley Tsang <stanley.tsang@amd.com>
2022-07-21 11:40:40 -0600

    Increase warmup iterations to 15

--- benchmark/benchmark_rocrand_generate.cpp
+++ benchmark/benchmark_rocrand_generate.cpp
@@ -91,7 +91,7 @@ void run_benchmark(const cli::Parser& parser,
     }
 
     // Warm-up
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < 15; i++)
     {
         ROCRAND_CHECK(generate_func(generator, data, size));
     }
