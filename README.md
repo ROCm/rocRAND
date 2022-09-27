95a2edf8529f2576379786dc3ab43fe1f0fdfda1
Mészáros Gergely <gergely@streamhpc.com>
2022-09-27 20:12:57 +0200

    Googlebenchmark integration (#277)
    * Fix CI
    
    * add clang format file and ci lint stage
    
    * Use googlebenchmark for the generate benchmark
    
    Instead of an ad-hoc reporting format, use the googlebenchmark library to define, filter and execute benchmarks.
    Custom context information is also added to the benchmark for archival reasons similar to rocPRIM.
    Googlebenchmark is automatically downloaded if not found when benchmarks are enabled.
    
    * Workaround broken hip headers included from host compiler in package test
    
    The hip headers have device only code visible to the host compiler on
    the nvcc path. Package test is compiled with just the host compiler
    because its only using the host api of rocrand. This workaround sets it
    to CUDA mode in the nvcc path until the fix for hip headers is released.
    
    Modernize to CUDA language instead of FindCuda.cmake.
    
    * Revert workaround for `ROCM_SYMLINK_LIBS`
    
    The underlying issue has been fixed in latest rocm-cmake.
    
    * Fix shebang of check-format.sh using env
    
    On some distributions (notable ones include alpine and nixos) `bash`
    is not part of the base installation, therefore its not installed in `/bin`.
    Use `env` to find bash in the `PATH` which is portable on most *nix
    systems.
    
    * Separate new and legacy benchmarks with a cmake option
    
    Restore the old `bencmark_rocrand_generate` for backwards compatibility.
    Add the googlebenchmark based version as `benchmark_rocrand_host_api`.
    Building the old benchmark can be enabled by setting `BUILD_LEGACY_BENCHMARK`
    to `ON` (on by default).
    A later change can change the default and then finally remove the old
    benchmarks giving users time to adjust to the changes.
    
    * Use the new benchmark name for the host api benchmark
    
    * Add deprecation for BUILD_LEGACY_BENCHMARK
    
    * Drop googlebenchmark compiler override
    
    The underlying issue has been fixed in v1.5.5 with
    https://github.com/google/benchmark/pull/1174. The workaround is no
    longer needed.
    
    * Fixes to enable benchmark build on Windows (#282)
    
    * add new generators to changed benchmarks
    
    * Windows googlebench ci fix (#294)
    
    * Update windows compiler for googlebenchmark
    
    * Fix line indentations
    
    Co-authored-by: Lőrinc Serfőző <lorinc@streamhpc.com>
    Co-authored-by: Nol Moonen <nol@streamhpc.com>
    Co-authored-by: Bibrak Qamar <bibrak@streamhpc.com>
    Co-authored-by: Máté Ferenc Nagy-Egri <mate@streamhpc.com>
    Co-authored-by: Stanley Tsang <stanley.tsang@amd.com>
    
    [ROCm/rocRAND commit: 622e20e34a8ce83be5bfcba77ef91faaf4428ac4]

--- README.md
+++ README.md
@@ -126,15 +126,24 @@ ctest
 # Go to rocRAND build directory
 cd rocRAND; cd build
 
-# To run benchmark for generate functions:
-# engine -> all, xorwow, mrg31k3p, mrg32k3a, mtgp32, philox, lfsr113, 
+# To run benchmark for the host generate functions:
+# The benchmarks are registered with Google Benchmark as `device_generate<engine,distribution>`, where
+# engine -> xorwow, mrg31k3p, mrg32k3a, mtgp32, philox, lfsr113, 
 #           sobol32, scrambled_sobol32, sobol64, scrambled_sobol64
-# distribution -> all, uniform-uint, uniform-uchar, uniform-ushort, 
+# distribution -> uniform-uint, uniform-uchar, uniform-ushort, 
 #                 uniform-half, uniform-float, uniform-double, 
 #                 normal-half, normal-float, normal-double,
 #                 log-normal-half, log-normal-float, log-normal-double, poisson
 # Further option can be found using --help
-./benchmark/benchmark_rocrand_generate --engine <engine> --dis <distribution>
+./benchmark/benchmark_rocrand_host_api
+# To run specific benchmarks:
+./benchmark/benchmark_rocrand_host_api --benchmark_filter=<regex>
+# For example to run benchmarks with engine sobol64:
+./benchmark_rocrand_host_api --benchmark_filter="device_generate<sobol64*"
+# To view all registered benchmarks:
+./benchmark_rocrand_host_api --benchmark_list_tests=true
+# The benchmark also supports user input:
+./benchmark_rocrand_host_api --size <number> --trials <number> --offset <number> --dimensions <number> --lambda <float float float ...>
 
 # To run benchmark for device kernel functions:
 # engine -> all, xorwow, mrg31k3p, mrg32k3a, mtgp32, philox, lfsr113, 
@@ -148,6 +157,14 @@ cd rocRAND; cd build
 ./benchmark/benchmark_curand_generate --engine <engine> --dis <distribution>
 ./benchmark/benchmark_curand_kernel --engine <engine> --dis <distribution>
 ```
+
+### Legacy benchmarks
+
+The legacy benchmarks (before the move to using googlebenchmark) can be disabled by setting the
+cmake option `BUILD_LEGACY_BENCHMARK` to `OFF`. For compatibility, this settings defaults to `ON`
+when `BUILD_BENCHMARK` is set.
+The legacy benchmarks are deprecated and will be removed in a future version once all benchmarks are
+migrated to the new framework.
 
 ## Running Statistical Tests
 
---
+++ benchmark/benchmark_rocrand_host_api.cpp
@@ -0,0 +1,343 @@
+// Copyright (c) 2017-2022 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "benchmark_utils.hpp"
+
+#include <rocrand/rocrand.h>
+
+// Google Benchmark
+#include <benchmark/benchmark.h>
+
+#include "cmdparser.hpp"
+
+#include <hip/hip_runtime.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+#ifndef DEFAULT_RAND_N
+const size_t DEFAULT_RAND_N = 1024 * 1024 * 128;
+#endif
+
+typedef rocrand_rng_type rng_type_t;
+template<typename T>
+using generate_func_type = std::function<rocrand_status(rocrand_generator, T*, size_t)>;
+
+template<typename T>
+void run_benchmark(benchmark::State&     state,
+                   generate_func_type<T> generate_func,
+                   const size_t          size,
+                   const size_t          trials,
+                   const size_t          dimensions,
+                   const size_t          offset,
+                   const rng_type_t      rng_type,
+                   hipStream_t           stream)
+{
+
+    const size_t rounded_size = (size / dimensions) * dimensions;
+
+    T* data;
+    HIP_CHECK(hipMalloc(&data, rounded_size * sizeof(T)));
+
+    rocrand_generator generator;
+    ROCRAND_CHECK(rocrand_create_generator(&generator, rng_type));
+
+    rocrand_status status = rocrand_set_quasi_random_generator_dimensions(generator, dimensions);
+    if(status != ROCRAND_STATUS_TYPE_ERROR) // If the RNG is not quasi-random
+    {
+        ROCRAND_CHECK(status);
+    }
+
+    ROCRAND_CHECK(rocrand_set_stream(generator, stream));
+
+    status = rocrand_set_offset(generator, offset);
+    if(status != ROCRAND_STATUS_TYPE_ERROR) // If the RNG is not pseudo-random
+    {
+        ROCRAND_CHECK(status);
+    }
+
+    // Warm-up
+    for(size_t i = 0; i < 15; i++)
+    {
+        ROCRAND_CHECK(generate_func(generator, data, rounded_size));
+    }
+    HIP_CHECK(hipDeviceSynchronize());
+
+    hipEvent_t start, stop;
+    HIP_CHECK(hipEventCreate(&start));
+    HIP_CHECK(hipEventCreate(&stop));
+    for(auto _ : state)
+    {
+        HIP_CHECK(hipEventRecord(start, stream));
+        for(size_t i = 0; i < trials; i++)
+        {
+            ROCRAND_CHECK(generate_func(generator, data, rounded_size));
+        }
+        HIP_CHECK(hipEventRecord(stop, stream));
+        HIP_CHECK(hipEventSynchronize(stop));
+
+        float elapsed = 0.0f;
+        HIP_CHECK(hipEventElapsedTime(&elapsed, start, stop));
+
+        state.SetIterationTime(elapsed / 1000.f);
+    }
+    state.SetBytesProcessed(trials * state.iterations() * rounded_size * sizeof(T));
+    state.SetItemsProcessed(trials * state.iterations() * rounded_size);
+
+    HIP_CHECK(hipEventDestroy(stop));
+    HIP_CHECK(hipEventDestroy(start));
+    ROCRAND_CHECK(rocrand_destroy_generator(generator));
+    HIP_CHECK(hipFree(data));
+}
+
+int main(int argc, char* argv[])
+{
+
+    // Parse argv
+    benchmark::Initialize(&argc, argv);
+
+    cli::Parser parser(argc, argv);
+    parser.set_optional<size_t>("size", "size", DEFAULT_RAND_N, "number of values");
+    parser.set_optional<size_t>("dimensions",
+                                "dimensions",
+                                1,
+                                "number of dimensions of quasi-random values");
+    parser.set_optional<size_t>("offset", "offset", 0, "offset of generated pseudo-random values");
+    parser.set_optional<size_t>("trials", "trials", 20, "number of trials");
+    parser.set_optional<std::vector<double>>(
+        "lambda",
+        "lambda",
+        {10.0},
+        "space-separated list of lambdas of Poisson distribution");
+    parser.run_and_exit_if_error();
+
+    hipStream_t stream;
+    HIP_CHECK(hipStreamCreate(&stream));
+
+    // Benchmark info
+    add_common_benchmark_info();
+
+    const size_t              size            = parser.get<size_t>("size");
+    const size_t              trials          = parser.get<size_t>("trials");
+    const size_t              dimensions      = parser.get<size_t>("dimensions");
+    const size_t              offset          = parser.get<size_t>("offset");
+    const std::vector<double> poisson_lambdas = parser.get<std::vector<double>>("lambda");
+
+    benchmark::AddCustomContext("size", std::to_string(size));
+    benchmark::AddCustomContext("trials", std::to_string(trials));
+    benchmark::AddCustomContext("dimensions", std::to_string(dimensions));
+    benchmark::AddCustomContext("offset", std::to_string(offset));
+
+    const std::map<rng_type_t, std::string> engine_type_map{
+        {          ROCRAND_RNG_PSEUDO_MTGP32,            "mtgp32"},
+        {          ROCRAND_RNG_PSEUDO_XORWOW,            "xorwow"},
+        {        ROCRAND_RNG_PSEUDO_MRG31K3P,          "mrg31k3p"},
+        {        ROCRAND_RNG_PSEUDO_MRG32K3A,          "mrg32k3a"},
+        {   ROCRAND_RNG_PSEUDO_PHILOX4_32_10,            "philox"},
+        {         ROCRAND_RNG_PSEUDO_LFSR113,           "lfsr113"},
+        {          ROCRAND_RNG_QUASI_SOBOL32,           "sobol32"},
+        {ROCRAND_RNG_QUASI_SCRAMBLED_SOBOL32, "scrambled_sobol32"},
+        {          ROCRAND_RNG_QUASI_SOBOL64,           "sobol64"},
+        {ROCRAND_RNG_QUASI_SCRAMBLED_SOBOL64, "scrambled_sobol64"},
+    };
+
+    const std::string benchmark_name_prefix = "device_generate";
+    // Add benchmarks
+    std::vector<benchmark::internal::Benchmark*> benchmarks = {};
+    for(std::pair<rng_type_t, std::string> engine : engine_type_map)
+    {
+
+        const rng_type_t  engine_type        = engine.first;
+        const std::string name_engine_prefix = benchmark_name_prefix + "<" + engine.second + ",";
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "uniform-uint>").c_str(),
+            &run_benchmark<unsigned int>,
+            [](rocrand_generator gen, unsigned int* data, size_t size_gen)
+            { return rocrand_generate(gen, data, size_gen); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "uniform-uchar>").c_str(),
+            &run_benchmark<unsigned char>,
+            [](rocrand_generator gen, unsigned char* data, size_t size_gen)
+            { return rocrand_generate_char(gen, data, size_gen); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "uniform-ushort>").c_str(),
+            &run_benchmark<unsigned short>,
+            [](rocrand_generator gen, unsigned short* data, size_t size_gen)
+            { return rocrand_generate_short(gen, data, size_gen); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "uniform-half>").c_str(),
+            &run_benchmark<__half>,
+            [](rocrand_generator gen, __half* data, size_t size_gen)
+            { return rocrand_generate_uniform_half(gen, data, size_gen); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(
+            benchmark::RegisterBenchmark((name_engine_prefix + "uniform-float>").c_str(),
+                                         &run_benchmark<float>,
+                                         [](rocrand_generator gen, float* data, size_t size_gen)
+                                         { return rocrand_generate_uniform(gen, data, size_gen); },
+                                         size,
+                                         trials,
+                                         dimensions,
+                                         offset,
+                                         engine_type,
+                                         stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "uniform-double>").c_str(),
+            &run_benchmark<double>,
+            [](rocrand_generator gen, double* data, size_t size_gen)
+            { return rocrand_generate_uniform_double(gen, data, size_gen); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "normal-half>").c_str(),
+            &run_benchmark<__half>,
+            [](rocrand_generator gen, __half* data, size_t size_gen)
+            { return rocrand_generate_normal_half(gen, data, size_gen, 0.0f, 1.0f); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "normal-float>").c_str(),
+            &run_benchmark<float>,
+            [](rocrand_generator gen, float* data, size_t size_gen)
+            { return rocrand_generate_normal(gen, data, size_gen, 0.0f, 1.0f); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "normal-double>").c_str(),
+            &run_benchmark<double>,
+            [](rocrand_generator gen, double* data, size_t size_gen)
+            { return rocrand_generate_normal_double(gen, data, size_gen, 0.0, 1.0); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "log-normal-half>").c_str(),
+            &run_benchmark<__half>,
+            [](rocrand_generator gen, __half* data, size_t size_gen)
+            { return rocrand_generate_log_normal_half(gen, data, size_gen, 0.0f, 1.0f); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "log-normal-float>").c_str(),
+            &run_benchmark<float>,
+            [](rocrand_generator gen, float* data, size_t size_gen)
+            { return rocrand_generate_log_normal(gen, data, size_gen, 0.0f, 1.0f); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        benchmarks.emplace_back(benchmark::RegisterBenchmark(
+            (name_engine_prefix + "log-normal-double>").c_str(),
+            &run_benchmark<double>,
+            [](rocrand_generator gen, double* data, size_t size_gen)
+            { return rocrand_generate_log_normal_double(gen, data, size_gen, 0.0, 1.0); },
+            size,
+            trials,
+            dimensions,
+            offset,
+            engine_type,
+            stream));
+
+        for(auto lambda : poisson_lambdas)
+        {
+            const std::string poisson_dis_name
+                = std::string("poisson(lambda=") + std::to_string(lambda) + ")";
+            benchmarks.emplace_back(benchmark::RegisterBenchmark(
+                (name_engine_prefix + poisson_dis_name).c_str(),
+                &run_benchmark<unsigned int>,
+                [lambda](rocrand_generator gen, unsigned int* data, size_t size_gen)
+                { return rocrand_generate_poisson(gen, data, size_gen, lambda); },
+                size,
+                trials,
+                dimensions,
+                offset,
+                engine_type,
+                stream));
+        }
+    }
+    // Use manual timing
+    for(auto& b : benchmarks)
+    {
+        b->UseManualTime();
+        b->Unit(benchmark::kMillisecond);
+    }
+    // Run benchmarks
+    benchmark::RunSpecifiedBenchmarks();
+    HIP_CHECK(hipStreamDestroy(stream));
+
+    return 0;
+}
