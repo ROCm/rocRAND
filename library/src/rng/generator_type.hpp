ead411ce24ed8a2834499ec49f630eed93963433
Beatriz <beatriz@streamhpc.com>
2022-09-28 11:52:12 +0000

    Reordering members of rocrand_generator_type

--- library/src/rng/generator_type.hpp
+++ library/src/rng/generator_type.hpp
@@ -44,7 +44,7 @@ struct rocrand_generator_type : public rocrand_generator_base_type
                            SeedType           seed   = SeedType{0},
                            unsigned long long offset = 0,
                            hipStream_t        stream = 0)
-        : base_type(GeneratorType), m_seed(seed), m_offset(offset), m_order(order), m_stream(stream)
+        : base_type(GeneratorType), m_order(order), m_seed(seed), m_offset(offset), m_stream(stream)
     {
 
     }
@@ -87,9 +87,9 @@ struct rocrand_generator_type : public rocrand_generator_base_type
     }
 
 protected:
+    rocrand_ordering   m_order;
     SeedType           m_seed;
     unsigned long long m_offset;
-    rocrand_ordering   m_order;
     hipStream_t        m_stream;
 };
 
