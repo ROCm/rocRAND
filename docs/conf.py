3bde72726fef528a6c6e9def9deed22038e505b5
Sam Wu <sjwu@ualberta.ca>
2023-03-16 12:36:23 -0600

    Documentation update for Read the Docs
    Standardize rocRAND documentation builds (#319)
    
    Add dependabot config and pin rocm-docs-core (#320)
    
    Co-authored-by: samjwu <samjwu@users.noreply.github.com>
    
    Fix dependabot file structure (#321)
    
    Co-authored-by: samjwu <samjwu@users.noreply.github.com>
    
    Removing redundant files (#326)
    
    Remove build dependency on Git (#327)
    
    The rocRAND project might use Git to download dependencies, but it is
    not required if the dependencies are already installed. And, even in
    the case that Git is needed to download dependencies, this find_package
    call remains unnecessary. The tooling that does the downloads will look
    for git itself.
    
    This change was first made in 7f71666d04e6252d7681d4e0360d4c9b70443ebc,
    but was reverted by 11cf251f0bb0374d0f1cbe48ffed121b8833d104.
    
    Update version string (#335)
    
    Merge pull request #337 from StreamHPC/fix-docs
    
    Fixed documentation build
    
    Update documentation requirements (#339)
    
    rocm-docs-core==0.13.4

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from rocm_docs import ROCmDocs
+import pathlib
+import sys
+
+# We need to add the location of the rocrand Python module to the PATH
+# in order to build the documentation of that module
+docs_dir_path = pathlib.Path(__file__).parent
+python_dir_path = docs_dir_path.parent / 'python' / 'rocrand'
+sys.path.append(str(python_dir_path))
+
+docs_core = ROCmDocs("hipBLAS Documentation")
+docs_core.run_doxygen()
+docs_core.setup()
+
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
