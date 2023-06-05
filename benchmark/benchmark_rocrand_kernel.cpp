83cd08290606e06957406034ac98d0f9f70341f5
RobsonRLemos <robson.lemos@amd.com>
2023-06-05 13:49:58 -0600

    Address cppcheck warnings to benchmark_rocrand_kernel

--- benchmark/benchmark_rocrand_kernel.cpp
+++ benchmark/benchmark_rocrand_kernel.cpp
@@ -258,6 +258,7 @@ struct runner<rocrand_state_lfsr113>
            const unsigned long long /* seed */,
            const unsigned long long /* offset */)
     {
+        this->dimensions = 0;
         const size_t states_size = blocks * threads;
         HIP_CHECK(hipMalloc(reinterpret_cast<void**>(&states),
                             states_size * sizeof(rocrand_state_lfsr113)));
@@ -919,7 +920,7 @@ int main(int argc, char *argv[])
     const std::string distribution_desc =
         "space-separated list of distributions:" +
         std::accumulate(all_distributions.begin(), all_distributions.end(), std::string(),
-            [](std::string a, std::string b) {
+            [](const std::string& a, const std::string& b) {
                 return a + "\n      " + b;
             }
         ) +
@@ -927,7 +928,7 @@ int main(int argc, char *argv[])
     const std::string engine_desc =
         "space-separated list of random number engines:" +
         std::accumulate(all_engines.begin(), all_engines.end(), std::string(),
-            [](std::string a, std::string b) {
+            [](const std::string& a, const std::string& b) {
                 return a + "\n      " + b;
             }
         ) +
@@ -938,8 +939,8 @@ int main(int argc, char *argv[])
     parser.set_optional<size_t>("trials", "trials", 20, "number of trials");
     parser.set_optional<size_t>("blocks", "blocks", 256, "number of blocks");
     parser.set_optional<size_t>("threads", "threads", 256, "number of threads in each block");
-    parser.set_optional<std::vector<std::string>>("dis", "dis", {"uniform-uint"}, distribution_desc.c_str());
-    parser.set_optional<std::vector<std::string>>("engine", "engine", {"philox"}, engine_desc.c_str());
+    parser.set_optional<std::vector<std::string>>("dis", "dis", {"uniform-uint"}, distribution_desc);
+    parser.set_optional<std::vector<std::string>>("engine", "engine", {"philox"}, engine_desc);
     parser.set_optional<std::vector<double>>("lambda", "lambda", {10.0}, "space-separated list of lambdas of Poisson distribution");
     parser.set_optional<std::string>("format", "format", {"console"}, "output format: console or csv");
     parser.run_and_exit_if_error();
