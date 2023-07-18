3b7f167f73b1faf8a8b1893361c01b8afcc587b1
Bence Parajdi <bence@streamhpc.com>
2023-07-18 13:58:20 +0000

    add explicit copy operator= for other param_types
    [ROCm/rocRAND commit: 0a7ba9b9a7900020ca0f54ea5c41104a94224f31]

--- library/include/rocrand/rocrand.hpp
+++ library/include/rocrand/rocrand.hpp
@@ -588,6 +588,7 @@ public:
 
         /// Copy constructor
         param_type(const param_type& params) = default;
+        param_type& operator=(const param_type& params) = default;
 
         /// \brief Returns the deviation distribution parameter.
         ///
@@ -788,6 +789,7 @@ public:
 
         /// Copy constructor
         param_type(const param_type& params) = default;
+        param_type& operator=(const param_type& params) = default;
 
         /// \brief Returns the mean distribution parameter.
         ///
