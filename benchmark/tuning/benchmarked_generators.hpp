0dd9a443212bdd401e7ff5a8ee6f532872fe34b6
Lőrinc Serfőző <lorinc@streamhpc.com>
2023-10-27 14:37:16 +0000

    Added config option, test, benchmarks to lfsr113
    [ROCm/rocRAND commit: 1848e8ae999600196ba3237b7d47831a5c866bb0]

--- benchmark/tuning/benchmarked_generators.hpp
+++ benchmark/tuning/benchmarked_generators.hpp
@@ -31,6 +31,9 @@
 #include "benchmark_tuning.hpp"
 
 template<class ConfigProvider>
+class rocrand_lfsr113_template;
+
+template<class ConfigProvider>
 class rocrand_philox4x32_10_template;
 
 template<class ConfigProvider>
@@ -40,6 +43,10 @@ namespace benchmark_tuning
 {
 
 template<>
+struct output_type_supported<unsigned long long, rocrand_lfsr113_template> : public std::false_type
+{};
+
+template<>
 struct output_type_supported<unsigned long long, rocrand_philox4x32_10_template>
     : public std::false_type
 {};
@@ -47,6 +54,9 @@ struct output_type_supported<unsigned long long, rocrand_philox4x32_10_template>
 template<>
 struct output_type_supported<unsigned long long, rocrand_xorwow_template> : public std::false_type
 {};
+
+extern template void add_all_benchmarks_for_generator<rocrand_lfsr113_template>(
+    std::vector<benchmark::internal::Benchmark*>& benchmarks, const benchmark_config& config);
 
 extern template void add_all_benchmarks_for_generator<rocrand_xorwow_template>(
     std::vector<benchmark::internal::Benchmark*>& benchmarks, const benchmark_config& config);
