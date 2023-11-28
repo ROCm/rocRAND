2ab4aef827313cb2b5f144cab89d84803f4b5cf3
Lőrinc Serfőző <lorinc@streamhpc.com>
2023-11-28 15:16:36 +0000

    Consolidate threefry
    [ROCm/rocRAND commit: 87021c4c4f0be537b95c0eb2fe4e88449230f183]

--- library/src/rng/cpp_utils.hpp
+++ library/src/rng/cpp_utils.hpp
@@ -24,22 +24,27 @@
 #ifndef ROCRAND_RNG_CPP_UTILS_HPP_
 #define ROCRAND_RNG_CPP_UTILS_HPP_
 
+#include "utils/unreachable.hpp"
+
 #include <hip/hip_runtime.h>
 
 #include <array>
-#include <cstddef>
 #include <limits>
 #include <tuple>
+#include <type_traits>
 #include <utility>
+
+#include <cassert>
+#include <cstddef>
 
 namespace cpp_utils
 {
 
-/// @brief Invoke a function on each element of a \c std::tuple separately.
-/// @tparam Functor Type of the function.
-/// @tparam Tuple Type of the \c std::tuple.
-/// @param fun Functor to invoke.
-/// @param tuple Tuple whose every element is passed to \c fun.
+/// \brief Invoke a function on each element of a \c std::tuple separately.
+/// \tparam Functor Type of the function.
+/// \tparam Tuple Type of the \c std::tuple.
+/// \param fun Functor to invoke.
+/// \param tuple Tuple whose every element is passed to \c fun.
 template<class Functor, class Tuple>
 constexpr void visit_tuple(Functor&& fun, Tuple&& tuple)
 {
@@ -59,16 +64,16 @@ struct array_size<std::array<T, N>>
     static constexpr inline std::size_t value = N;
 };
 
-/// @brief Returns the element count (size) of an \ref std::array.
-/// @tparam T Type of the \ref std::array.
+/// \brief Returns the element count (size) of an \ref std::array.
+/// \tparam T Type of the \ref std::array.
 template<class T>
 constexpr std::size_t array_size_v = array_size<T>::value;
 
-/// @brief Generates all combinations of values of the input arrays.
-/// @tparam T Element type of the input arrays.
-/// @tparam ...Ns Sizes of the input arrays.
-/// @param ...inputs Input array args.
-/// @return An array of arrays. Each array in the returned array contains
+/// \brief Generates all combinations of values of the input arrays.
+/// \tparam T Element type of the input arrays.
+/// \tparam ...Ns Sizes of the input arrays.
+/// \param ...inputs Input array args.
+/// \return An array of arrays. Each array in the returned array contains
 /// one unique combination of the values from the input arrays.
 template<class T, std::size_t... Ns>
 constexpr auto numeric_combinations(const std::array<T, Ns>... inputs)
@@ -102,7 +107,7 @@ constexpr auto numeric_combinations(const std::array<T, Ns>... inputs)
     return ret;
 }
 
-/// @brief Calculates greatest common divisor.
+/// \brief Calculates greatest common divisor.
 __host__ __device__ constexpr unsigned int gcd(const unsigned int a, const unsigned int b)
 {
     if(a == 0)
@@ -112,13 +117,137 @@ __host__ __device__ constexpr unsigned int gcd(const unsigned int a, const unsig
     return gcd(b, a % b);
 }
 
-/// @brief Calculates least common multiple
+/// \brief Calculates least common multiple
 __host__ __device__ constexpr unsigned int lcm(const unsigned int a, const unsigned int b)
 {
     if(a == 0 || b == 0)
         return 0;
     return a / gcd(a, b) * b;
 }
+
+/// \brief Whether the type argument is a HIP vector type, e.g. `float4`.
+template<class V>
+constexpr bool is_vector_type_v = false;
+
+template<class V>
+struct vector_size
+{
+    // Always false
+    static_assert(sizeof(V) == 0, "vector_size must be used with a vector type");
+};
+
+/// \brief The element count of the vector type, e.g. 2 for `float2`.
+template<class V>
+constexpr size_t vector_size_v = vector_size<V>::value;
+
+template<class V>
+struct vector_element
+{
+    // Always false
+    static_assert(sizeof(V) == 0, "vector_element must be used with a vector type");
+};
+
+/// \brief The element type of the vector type, e.g. `float` for `float2`.
+template<class V>
+using vector_element_t = typename vector_element<V>::type;
+
+#define INSTANTIATE_VECTOR_TYPE_TRAITS(V, T, N)       \
+    template<>                                        \
+    constexpr inline bool is_vector_type_v<V> = true; \
+    template<>                                        \
+    struct vector_size<V>                             \
+    {                                                 \
+        static constexpr inline size_t value = N;     \
+    };                                                \
+    template<>                                        \
+    struct vector_element<V>                          \
+    {                                                 \
+        using type = T;                               \
+    };
+
+#define INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(base, T) \
+    INSTANTIATE_VECTOR_TYPE_TRAITS(base##2, T, 2)           \
+    INSTANTIATE_VECTOR_TYPE_TRAITS(base##4, T, 4)
+
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(uchar, unsigned char)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(ushort, unsigned short)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(uint, unsigned int)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(ulong, unsigned long)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(ulonglong, unsigned long long int)
+
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(char, char)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(short, short)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(int, int)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(long, long)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(longlong, long long int)
+
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(float, float)
+INSTANTIATE_VECTOR_TYPE_TRAITS_FOR_ELEMENT(double, double)
+
+#undef INSTANTIATE_VECTOR_TYPE_TRAITS
+
+/// \brief Wraps an instance of a vector type and provides indexing operators.
+/// \tparam V The wrapped vector type.
+template<class V>
+struct vec_wrapper
+{
+    static_assert(is_vector_type_v<V>, "vec_wrapper can only be used with vector types");
+
+    __host__ __device__ explicit vec_wrapper(V vec) : m_vec(vec) {}
+
+    template<class U = V, std::enable_if_t<vector_size_v<U> == 2, int> = 0>
+    __host__ __device__ auto& operator[](int idx)
+    {
+        switch(idx)
+        {
+            case 0: return m_vec.x;
+            case 1: return m_vec.y;
+        }
+        ROCRAND_UNREACHABLE("Out-of-bounds indexing of vector");
+    }
+
+    template<class U = V, std::enable_if_t<vector_size_v<U> == 2, int> = 0>
+    __host__ __device__ const auto& operator[](int idx) const
+    {
+        switch(idx)
+        {
+            case 0: return m_vec.x;
+            case 1: return m_vec.y;
+        }
+        ROCRAND_UNREACHABLE("Out-of-bounds indexing of vector");
+    }
+
+    template<class U = V, std::enable_if_t<vector_size_v<U> == 4, int> = 0>
+    __host__ __device__ auto& operator[](int idx)
+    {
+        switch(idx)
+        {
+            case 0: return m_vec.x;
+            case 1: return m_vec.y;
+            case 2: return m_vec.z;
+            case 3: return m_vec.w;
+        }
+        ROCRAND_UNREACHABLE("Out-of-bounds indexing of vector");
+    }
+
+    template<class U = V, std::enable_if_t<vector_size_v<U> == 4, int> = 0>
+    __host__ __device__ const auto& operator[](int idx) const
+    {
+        switch(idx)
+        {
+            case 0: return m_vec.x;
+            case 1: return m_vec.y;
+            case 2: return m_vec.z;
+            case 3: return m_vec.w;
+        }
+        ROCRAND_UNREACHABLE("Out-of-bounds indexing of vector");
+    }
+
+    V m_vec;
+};
+
+template<class V>
+vec_wrapper(V) -> vec_wrapper<V>;
 
 } // end namespace cpp_utils
 
