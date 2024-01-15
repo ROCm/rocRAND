9375ae9900a6e24151a306f55af442cac429f599
Sam Wu <sam.wu2@amd.com>
2024-01-15 11:34:00 -0700

    Standardize documentation for ReadtheDocs (#420)

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,11 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from rocm_docs import ROCmDocs
 import pathlib
+import re
 import sys
+
+from rocm_docs import ROCmDocs
 
 # We need to add the location of the rocrand Python module to the PATH
 # in order to build the documentation of that module
@@ -14,11 +16,27 @@ docs_dir_path = pathlib.Path(__file__).parent
 python_dir_path = docs_dir_path.parent / 'python' / 'rocrand'
 sys.path.append(str(python_dir_path))
 
-external_projects_current_project = "rocrand"
+with open('../CMakeLists.txt', encoding='utf-8') as f:
+    match = re.search(r'rocm_setup_version\( VERSION\s+\"?([0-9.]+)[^0-9.]+', f.read())
+    if not match:
+        raise ValueError("VERSION not found!")
+    version_number = match[1]
+left_nav_title = f"rocRAND {version_number} Documentation"
 
-docs_core = ROCmDocs("rocRAND Documentation")
-docs_core.run_doxygen()
+# for PDF output on Read the Docs
+project = "rocRAND Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(left_nav_title)
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.setup()
+
+external_projects_current_project = "rocrand"
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
