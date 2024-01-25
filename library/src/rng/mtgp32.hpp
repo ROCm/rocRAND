af83161fbb3d63d98eeb3c26b1b35bbe63d0d986
Lőrinc Serfőző <lorinc@streamhpc.com>
2024-01-25 09:36:50 +0000

    Added mtgp32 dynamic ordering config

--- library/src/rng/mtgp32.hpp
+++ library/src/rng/mtgp32.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2023 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2017-2024 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -66,6 +66,7 @@
 #include <rocrand/rocrand_mtgp32_11213.h>
 
 #include "config/config_defaults.hpp"
+#include "config/mtgp32_config.hpp"
 #include "config_types.hpp"
 #include "device_engines.hpp"
 #include "distributions.hpp"
