60dcc8713379076d8e50c75d289295864ea0f4f7
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2024-02-08 08:51:33 +0000

    Re-tuned lfsr113 for gfx906

--- library/src/rng/config/lfsr113_config.hpp
+++ library/src/rng/config/lfsr113_config.hpp
@@ -50,7 +50,7 @@ struct generator_config_selector<ROCRAND_RNG_PSEUDO_LFSR113, T>
         switch(arch)
         {
             case target_arch::gfx1030: return 512;
-            case target_arch::gfx906: return 560;
+            case target_arch::gfx906: return 2048;
             case target_arch::gfx908: return 1024;
             default: return generator_config_defaults<ROCRAND_RNG_PSEUDO_LFSR113, T>::blocks;
         }
