8fcf39628e76742ea3c8fade1b3c26f329ded8ce
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-02-22 11:50:03 -0700

    Switch from using hipcc to using Clang compiler initial implementation (#434)
    [ROCm/rocRAND commit: b567a3cc6b737700c6c2ba9e59e3f42148d0c2ab]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -20,7 +20,7 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
                 mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
                 # gfxTargetParser reads gfxarch and adds target features such as xnack
                 ${auxiliary.gfxTargetParser()}
-                ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc ${buildTypeArg} ${buildStatic} ${amdgpuTargets} ${codeCovFlag} -DBUILD_TEST=ON -DBUILD_BENCHMARK=ON ../..
+                ${cmake} --toolchain=toolchain-linux.cmake ${buildTypeArg} ${buildStatic} ${amdgpuTargets} ${codeCovFlag} -DBUILD_TEST=ON -DBUILD_BENCHMARK=ON ../..
                 make -j\$(nproc)
                 """
 
