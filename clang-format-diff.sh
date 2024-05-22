c680f02dd155446bcfa116e8cf4959cbadbfa9df
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-05-22 15:08:05 -0600

    Run clang-format instead of using Python script

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -5,9 +5,7 @@ DIFF=$(git diff -U0 HEAD)
 echo "$DIFF" > changes.diff
 
 # Apply clang-format on diff
-curl -LO https://raw.githubusercontent.com/llvm/llvm-project/main/clang/tools/clang-format/clang-format-diff.py
-chmod +x clang-format-diff.py
-FORMAT_DIFF=$(./clang-format-diff.py changes.diff)
+FORMAT_DIFF=$(clang-format -style=file changes.diff)
 
 if [ ! -z "$FORMAT_DIFF" ]; then
     echo "The following formatting errors were found:"
