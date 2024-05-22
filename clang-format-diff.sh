8db6a558dd93366c8c706b7eb83f65ecafaa53db
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-05-22 16:24:23 -0600

    Compare branches

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+BASE_BRANCH=$1
+CURRENT_BRANCH=$2
+
 # Save unified diff with 0 context
-git diff -U0 HEAD > changes.diff
+git diff -U0 --name-only $BASE_BRANCH...$CURRENT_BRANCH > changes.diff
 
 # Apply clang-format on diff
 FORMAT_DIFF=$(clang-format -style=file changes.diff)
