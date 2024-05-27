f8516928090fe63ceb1342a960de3977a6926c3a
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-05-27 15:01:21 -0600

    Make suggestions on .h and .cpp files

--- clang-format-apply.sh
+++ clang-format-apply.sh
@@ -6,5 +6,13 @@ CURRENT_BRANCH=$2
 # Save unified diff with 0 context
 git diff -U0 --name-only $BASE_BRANCH...$CURRENT_BRANCH > changes.diff
 
-# Apply clang-format on diff
-FORMAT_DIFF=$(clang-format -i -style=file changes.diff)
+cat changes.diff
+
+# Run clang-format in-place on .h and .cpp files
+while IFS= read -r line; do
+  if [ -n "$line" ]; then
+    if [[ "$line" == *.h ]] || [[ "$line" == *.cpp ]]; then
+      clang-format -i "$line"
+    fi
+  fi
+done < changes.diff
--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -6,6 +6,8 @@ CURRENT_BRANCH=$2
 # Save unified diff with 0 context
 git diff -U0 --name-only $BASE_BRANCH...$CURRENT_BRANCH > changes.diff
 
+cat changes.diff
+
 # Apply clang-format on diff
 FORMAT_DIFF=$(clang-format -style=file changes.diff)
 
