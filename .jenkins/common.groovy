d7d21fc8c1b99b7fa96bfcb19b612c6033ecb45f
ForLoopsAreCool <john@robbinshba.com>
2025-05-15 15:45:35 -0600

    Update common.groovy

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -17,6 +17,7 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
 
     def command = """#!/usr/bin/env bash
                 set -x
+                echo "I LIKE FOR LOOPS"
                 cd ${project.paths.project_build_prefix}
                 mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
                 # gfxTargetParser reads gfxarch and adds target features such as xnack
