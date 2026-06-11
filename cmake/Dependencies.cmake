0613ea901394e4c9b246fcf0b5b9cc42fd3bc3b3
Saiyang Zhang <41819448+Saiyang-Zhang@users.noreply.github.com>
2026-06-11 20:40:22 +0000

    [rocm-libraries] ROCm/rocm-libraries#6573 (commit 7f54c6a)
    Replace download project with fetchcontent
    
    ## Motivation
    
    The `download_project` funtion used to fetch libraries is outdated, we
    should use modern CMake `FetchContent`.
    
    ## Technical Details
    
    Removed `DownloadProject.cmake`, `DownloadProject.CMakeLists.cmake.in`
    which defines the `download_project` function.
    
    Add `fetc_content_isolated` which uses `FetchContent` to replace
    `download_project`, avoiding parent project flags penetrating into
    sub-libraries.
    
    Removed `LIST_SEPARATOR` replacing `;` with `|`, because cmake can parse
    it correctly. with `CMAKE_ARGS "-DGPU_TARGETS=${GPU_TARGETS}"`.
    
    Removed `INSTALL_DIR` relevant because `FetchContent_MakeAvailable` does
    the compilation and doesn't need location specification.
    
    Removed `LOG_*` arguments because they are skipped by
    `FetchContent_MakeAvailable`.
    
    Add `target_compile_options(generate_resource_spec PRIVATE
    -Wno-unused-command-line-argument)` because test fail with `-Werror` for
    unused `--offload-compress`, which is not necessary.
    
    The method in
    https://github.com/ROCm/rocm-libraries/commit/efd316edd19d13bd7eaf91160c2960055ed6fd42
    worked but not in
    https://github.com/ROCm/rocm-libraries/commit/5f26be8f12ccc1e972917df2da00bc7fcc06d139.
    An alternative method is used instead.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2018-2025 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2018-2026 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -39,6 +39,15 @@ if(DEFINED BUILD_SHARED_LIBS)
   set(USER_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
 endif()
 set(USER_ROCM_WARN_TOOLCHAIN_VAR ${ROCM_WARN_TOOLCHAIN_VAR})
+
+# Suppress ROCmChecks warnings for local toolchain modifications.
+set(ROCM_WARN_TOOLCHAIN_VAR OFF)
+
+# Force older versions of option() in googletest to respect the local variable setting.
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+# Resolve Ninja generator errors regarding RPATH relinking during the install phase for merged subprojects.
+set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
 
 # Change variables before configuring dependencies
 set(ROCM_WARN_TOOLCHAIN_VAR OFF CACHE BOOL "")
@@ -79,14 +88,21 @@ if(NOT ROCmCMakeBuildTools_FOUND)
   else()
     set(SOURCE_SUBDIR_ARG)
   endif()
-  include(FetchContent)
-  FetchContent_Declare(
+  include(cmake/FetchContentIsolated.cmake)
+  fetch_content_isolated(
     rocm-cmake
     GIT_REPOSITORY https://github.com/ROCm/rocm-cmake.git
     GIT_TAG        rocm-6.4.4
     ${SOURCE_SUBDIR_ARG}
   )
-  FetchContent_MakeAvailable(rocm-cmake)
+  execute_process(
+    WORKING_DIRECTORY ${rocm-cmake_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} ${rocm-cmake_SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=.
+  )
+  execute_process(
+    WORKING_DIRECTORY ${rocm-cmake_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} --build ${rocm-cmake_SOURCE_DIR} --target install
+  )
   find_package(ROCmCMakeBuildTools CONFIG REQUIRED NO_DEFAULT_PATH PATHS "${rocm-cmake_SOURCE_DIR}")
 else()
   find_package(ROCmCMakeBuildTools 0.7.3 CONFIG REQUIRED PATHS "${ROCM_ROOT}")
@@ -100,8 +116,7 @@ include(ROCMInstallSymlinks)
 include(ROCMCheckTargetIds)
 include(ROCMClients)
 
-# For downloading and building required dependencies
-include(FetchContent)
+include(cmake/FetchContentIsolated.cmake)
 # Test dependencies
 if(BUILD_TEST)
   # Google Test (https://github.com/google/googletest)
@@ -119,15 +134,12 @@ if(BUILD_TEST)
 
   if(NOT TARGET GTest::GTest AND NOT TARGET GTest::gtest)
     message(STATUS "Google Test not found or force download on. Fetching...")
-    option(BUILD_GTEST "Builds the googletest subproject" ON)
-    option(BUILD_GMOCK "Builds the googlemock subproject" OFF)
-    option(INSTALL_GTEST "Enable installation of googletest" ON)
-    FetchContent_Declare(
+    fetch_content_isolated(
       googletest
       GIT_REPOSITORY https://github.com/google/googletest.git
       GIT_TAG        v1.15.2
+      CMAKE_ARGS     -DBUILD_GTEST=ON -DBUILD_GMOCK=OFF -DINSTALL_GTEST=ON
     )
-    FetchContent_MakeAvailable(googletest)
   endif()
 endif()
 
@@ -140,15 +152,16 @@ if(BUILD_BENCHMARK)
 
   if(NOT TARGET benchmark::benchmark)
     message(STATUS "Google Benchmark not found or force download on. Fetching...")
-    option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library" OFF)
-    option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark" OFF)
-    FetchContent_Declare(
+
+    set(HAVE_STD_REGEX ON)
+    set(RUN_HAVE_STD_REGEX 1)
+
+    fetch_content_isolated(
       googlebenchmark
       GIT_REPOSITORY https://github.com/google/benchmark.git
       GIT_TAG        v1.9.1
+      CMAKE_ARGS     -DBENCHMARK_ENABLE_TESTING=OFF -DBENCHMARK_ENABLE_INSTALL=OFF
     )
-    set(HAVE_STD_REGEX ON)
-    set(RUN_HAVE_STD_REGEX 1)
     FetchContent_MakeAvailable(googlebenchmark)
   endif()
 endif()
--- test/cpp_wrapper/cmake/Dependencies.cmake
+++ test/cpp_wrapper/cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2024-2026 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@ cmake_minimum_required(VERSION 3.16)
 # Dependencies
 
 # For downloading and building required dependencies
-include(FetchContent)
+include(cmake/FetchContentIsolated.cmake)
 
 # Google Test (https://github.com/google/googletest)
 # NOTE: Google Test has created a mess with legacy FindGTest.cmake and newer GTestConfig.cmake
@@ -42,13 +42,10 @@ endif()
 
 if(NOT TARGET GTest::GTest AND NOT TARGET GTest::gtest)
   message(STATUS "Google Test not found or force download on. Fetching...")
-  option(BUILD_GTEST "Builds the googletest subproject" ON)
-  option(BUILD_GMOCK "Builds the googlemock subproject" OFF)
-  option(INSTALL_GTEST "Enable installation of googletest" OFF)
-  FetchContent_Declare(
+  fetch_content_isolated(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG        v1.15.2
+    CMAKE_ARGS     -DBUILD_GTEST=ON -DBUILD_GMOCK=OFF -DINSTALL_GTEST=OFF
   )
-  FetchContent_MakeAvailable(googletest)
 endif()
